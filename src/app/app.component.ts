a4a36b79272e6dd7fc0a57f1266a9c6c222db35e
Simona Marcu <67797490+simonamarcu2@users.noreply.github.com>
2022-03-29 11:34:29 +0100

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  template:`<p>Hello World</p>`,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
